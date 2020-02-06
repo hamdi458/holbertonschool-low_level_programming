f53c1f7ad9b12a3ea3c4d28287387f4541b6188c
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-02-06 16:01:52 +0000

    hexa

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,8 +9,8 @@ int main(void)
 {
 char c;
 int i;
-for (i = 0; i <= 9; i++)                                                                                        
-putchar(i + '0'); 
+for (i = 0; i <= 9; i++)
+putchar(i + '0');
 for (c = 'a'; c <= 'f'; ++c)
 putchar(c);
 putchar('\n');
