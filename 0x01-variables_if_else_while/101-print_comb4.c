4d0586f4e73fdf054f0c609177f299e110e7a82a
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-02-06 18:57:14 +0000

    aa

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,43 +7,42 @@
 
 int main(void)
 {
-  int c;
-  int x;
-  int y;
-  int z;
+int c;
+int x;
+int y;
+int z;
 for (c = 12; c <= 789; c++)
 {
 int k = 0;
 x = c / 100;
 y = c % 100 / 10;
 z = c % 10;
-if(x != y && x != z && y != z)
+if (x != y && x != z && y != z)
 {
-if(z < y && z < x)
+if (z < y && z < x)
 {
 k = k + z * 100;
-if(x < y)
-k=k + x * 10 + y;
+if (x < y)
+k = k + x * 10 + y;
 else
-k=k + y * 10 + x;
-} 
-
-if(y < z && y < x) 
-{
-k = k + y * 100;
-if(z < x)
-k=k + z * 10 + x;
-else
-k=k + x * 10 + z ;
+k = k + y * 10 + x;
 }
 
-if(x < z && x < y)
+if (y < z && y < x)
+{
+k = k + y * 100;
+if (z < x)
+k = k + z * 10 + x;
+else
+k = k + x * 10 + z;
+}
+if (x < z && x < y)
 {
 k = k + x * 100;
-if(z < y)
-k=k + z * 10 + y;
+if (z < y)
+k = k + z * 10 + y;
 else
-k=k + y * 10 + z;                                                                                      
+k = k + y * 10 + z;
 }
 }
 putchar(k / 100 + '0');
