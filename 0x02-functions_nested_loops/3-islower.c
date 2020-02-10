24b23919a410e00ba2d7d4cd87148db18580515f
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-02-10 21:34:36 +0000

    lopps

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,15 @@
 #include "holberton.h"
+/**
+ * _islower - check the code for Holberton School students.
+ *
+ * @ch: Variable containing the letter to be checked
+ * Return: Always 0.
+ */
 int _islower(int ch)
 {
-  int c=ch;
-  if ( c>=65 && c<=90)
-    return (0);
-    else
-      return (1);
+int c = ch;
+if (c >= 65 && c <= 90)
+return (0);
+else
+return (1);
 }
