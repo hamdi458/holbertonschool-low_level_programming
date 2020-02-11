6d27ac77941a7c687834a2b45dff3a1209465859
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-02-11 10:36:22 +0000

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - adds two integers and returns the result
+ * @i: Variable containing integer
+ * @j: Variable containing integer
+ * Return: addition i+j
+ *
+ */
+int add(int i, int j)
+{
+return (i + j);
+}
