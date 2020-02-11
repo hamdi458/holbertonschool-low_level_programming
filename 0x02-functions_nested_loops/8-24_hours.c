f6cffb122563ab82a252ffbed521c2e934fb716c
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-02-11 08:53:14 +0000

    hm

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,6 +8,7 @@ void jack_bauer(void)
 {
 int i, j;
 for (i = 0; i <= 23; i++)
+{
 for (j = 0; j <= 59; j++)
 {
 _putchar(i / 10 + '0');
@@ -17,5 +18,6 @@ _putchar(':');
 _putchar(j / 10 + '0');
 _putchar(j % 10 + '0');
 _putchar('\n');
+}
 }
 }
