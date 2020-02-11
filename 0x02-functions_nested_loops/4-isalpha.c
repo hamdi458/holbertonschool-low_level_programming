9e7f4b523acd0aee425f04904779e77f2f57b9c4
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-02-11 08:34:46 +0000

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,12 @@
 #include "holberton.h"
 /**
  * _isalpha - checks for alphabetic characters
- * @c: Variable containing the letter to be checked
  *
  * Return: 1 if c is a letter, 0 not alpha
  *
  */
-int _isalpha(int)
+int _isalpha(int c)
 {
-int c = ch;
 if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 return (1);
 else
