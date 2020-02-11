4bf009909733080dab1398eb114f8102d5f07385
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-02-11 08:27:22 +0000

    c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,10 @@
 #include "holberton.h"
+/**
+ * print_sign - check the number
+ * @n: number recieved
+ *
+ *Return: 1 greater than zero  0 if is zero -1 if less than zero
+ */
 int print_sign(int n)
 {
 if (n > 0)
