e56eecd7d27978c816ca691d14eaa73e1ad2e884
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-02-11 09:04:19 +0000

    hm

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,22 @@
 #include "holberton.h"
+
 /**
  * jack_bauer - print every minute of the day of Jack Bauer
  *
  * Return: blabla
  */
+
 void jack_bauer(void)
 {
 int i, j;
 for (i = 0; i <= 23; i++)
-{
 for (j = 0; j <= 59; j++)
 {
 _putchar(i / 10 + '0');
 _putchar(i % 10 + '0');
-_putchar(' ');
 _putchar(':');
 _putchar(j / 10 + '0');
 _putchar(j % 10 + '0');
 _putchar('\n');
-}
 }
 }
