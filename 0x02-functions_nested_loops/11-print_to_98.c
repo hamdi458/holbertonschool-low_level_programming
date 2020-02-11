b5e6052339abdf27134e48e72f346723fc1c5493
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-02-11 11:11:28 +0000

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - adds two integers and returns the result
+ * @n: Variable containing integer
+ *
+ * Return: 0
+ *
+ */
+void print_to_98(int n)
+{
+int i;
+if (n == 98)
+{
+printf("%d\n", n);
+}
+else
+if (n > 98)
+{
+for (i = n; i >= 98; i--)
+{
+printf("%d", i);
+if (i > 98)
+printf(", ");
+}
+printf("\n");
+}
+else
+{
+for (i = n; i <= 98; i++)
+{
+printf("%d", i);
+if (i < 98)
+printf(", ");
+}
+printf("\n");
+}
+}
