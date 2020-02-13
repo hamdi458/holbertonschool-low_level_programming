c923c31b9ac694c3ad93a72ce70eb381d288c4e0
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-02-13 12:36:03 +0000

    function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two integers and returns the result
+ * @a: Variable containing integer
+ * @b: Variable containing integer
+ * Return: addition i+j
+ *
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -8,6 +8,6 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int mul(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
