f1a4ece70ea1aa1809c0d0a5f849c8d898fcf661
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-02-13 19:38:59 +0000

    function that prints a triangle, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_square - prints the numbers, from 0 to 9.
+ *@size : the number of #
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+int i;
+int j;
+int k;
+if (size <= 0)
+_putchar('\n');
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size-i; j++)
+_putchar(' ');
+for (k = 1; k <= i; k++)
+_putchar('#');
+_putchar('\n');
+}
+}
