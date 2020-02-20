bbb1fbea1ab5fc204fe6fa7aa9fd1c509c1a3d12
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-02-20 16:53:22 +0000

    aa

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * string_toupper - upper
+ * @c: chaine
+ *
+ * Return:0
+ */
+char *string_toupper(char *c)
+{
+int i = 0;
+while (c[i] != '\0')
+{
+if (c[i] >= 'a' && c[i] <= 'z')
+c[i] =c[i] -32;
+i++;
+}
+return (c);
+}
