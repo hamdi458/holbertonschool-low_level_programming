a8e4e790891a2c044f799d8033457366e274b8e5
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-02-21 17:37:34 +0000

    function that prints half of a string, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * puts_half - Prints a string followed by a new line to stdout
+ * @str: The string to be printed
+ */
+
+void puts_half(char *str)
+{
+  int l = 0;
+  int n = 0;
+
+  while (str[n++])
+    {
+      l++;
+    }
+  if (l % 2 == 0)
+    {
+      l = l / 2;
+    }
+  else
+    {
+      l = (l - 1) / 2;
+      l = (l + 1);
+    }
+  n = l;
+  while (str[l++])
+    {
+      _putchar(str[n++]);
+    }
+  _putchar('\n');
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,4 +1,4 @@
-void reset_to_98(int *n);
+Bvoid reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -6,3 +6,4 @@ int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
