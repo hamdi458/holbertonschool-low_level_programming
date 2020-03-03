b255a5a4f1a6690dba2e3f90066e2bdcd3b53a08
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-03 17:41:14 +0000

    aa

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,12 +13,12 @@ char *_strdup(char *str)
 char *ar;
 int i = 0;
 int j = 0;
-while(str[i])
+while (str[i])
 i++;
 i++;
-ar = malloc(i * sizeof(char));
+ar = malloc(i);
 if (str == NULL || ar == 0)
-return(NULL);
+return (NULL);
 for (j = 0; j < i; j++)
 {
 ar[j] = str[j];
