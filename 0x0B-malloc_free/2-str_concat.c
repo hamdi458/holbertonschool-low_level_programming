a05126e52a9dd8f110c3b64e4c8a1b0874a6a937
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-04 13:08:00 +0000

    function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ int j, i, size1, size2;
 size1 = 0;
 size2 = 0;
 if (s1 == NULL)
-s1="";
+s1 = "";
 if (s2 == NULL)
-s2="";
+s2 = "";
 while (s1[size1] != '\0')
 size1++;
 while (s2[size2] != '\0')
