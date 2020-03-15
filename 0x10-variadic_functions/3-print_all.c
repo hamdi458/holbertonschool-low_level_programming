3c2e589658ef767ad65df525887183a1e72db0b9
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-15 18:52:30 +0000

    aa

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -18,13 +18,13 @@ while (format[i] && format)
 switch (format[i])
 {
 case 'c':
-printf("%c", (char) va_arg(ap, int));
+printf("%c", va_arg(ap, int));
 break;
 case 'i':
 printf("%d", va_arg(ap, int));
 break;
 case 'f':
-printf("%f", (float) va_arg(ap, double));
+printf("%f", va_arg(ap, double));
 break;
 case 's':
 ch = va_arg(ap, char *);
