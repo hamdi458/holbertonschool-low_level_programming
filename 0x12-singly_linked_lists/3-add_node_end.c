284a184f298907c9b20daad9b0ca27cc4c0186e4
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-20 22:36:58 +0000

    second

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *add_node_end -  adds a new node at the end of a list_t list.
+ *@head : head of linked list
+ *@str : str of node
+ *Return: new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+  int s = 0;
+  list_t *nouveau = malloc(sizeof(*nouveau)), *suivant;
+  if (nouveau == NULL)
+    return (NULL);
+  if (!str)
+    {
+      nouveau->str = NULL;
+      nouveau->len = 0;
+    }
+  else
+    {
+      nouveau->str = strdup(str);
+      while (*str != '\0')
+	{
+	  s++;
+	  str++;
+	}
+      nouveau->len = s;  
+    }
+  nouveau->next = NULL;
+  if (*head == NULL)
+    {
+      *head = nouveau;
+      return (nouveau);
+    }
+  suivant = *head;
+  while (suivant->next != NULL)
+   suivant = suivant->next;
+  suivant->next = nouveau;
+  return (nouveau);
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *free_list -  delete a list_t list.
+ *@head : head of linked list
+ *Return: 0
+ */
+void free_list(list_t *head)
+{
+list_t *asup;
+while(head)
+{
+asup = head;
+head = head->next;
+free(asup);
+}
+}
