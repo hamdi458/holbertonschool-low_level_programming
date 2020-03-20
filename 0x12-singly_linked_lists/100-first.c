4160952f68a74c9f05ecdc21259573143c1cd7c0
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-20 23:35:56 +0000

    3

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
-void before_main() __attribute__((constructor));
-void before_main() 
+void before_main(void) __attribute__((constructor));
+/**
+ *before_main - executed fun before the main
+ */
+void before_main(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, ");
+printf("you must allow,\nI bore my house upon my back!\n");
 }
