8bc0cd357e318b276872dec62e770850b285a76c
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-20 23:18:37 +0000

    --

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,34 +10,34 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-  int s = 0;
-  list_t *nouveau = malloc(sizeof(*nouveau)), *suivant;
-  if (nouveau == NULL)
-    return (NULL);
-  if (!str)
-    {
-      nouveau->str = NULL;
-      nouveau->len = 0;
-    }
-  else
-    {
-      nouveau->str = strdup(str);
-      while (*str != '\0')
-	{
-	  s++;
-	  str++;
-	}
-      nouveau->len = s;  
-    }
-  nouveau->next = NULL;
-  if (*head == NULL)
-    {
-      *head = nouveau;
-      return (nouveau);
-    }
-  suivant = *head;
-  while (suivant->next != NULL)
-   suivant = suivant->next;
-  suivant->next = nouveau;
-  return (nouveau);
+int s = 0;
+list_t *nouveau = malloc(sizeof(*nouveau)), *suivant;
+if (nouveau == NULL)
+return (NULL);
+if (!str)
+{
+nouveau->str = NULL;
+nouveau->len = 0;
+}
+else
+{
+nouveau->str = strdup(str);
+while (*str != '\0')
+{
+s++;
+str++;
+}
+nouveau->len = s;
+}
+nouveau->next = NULL;
+if (*head == NULL)
+{
+*head = nouveau;
+return (nouveau);
+}
+suivant = *head;
+while (suivant->next != NULL)
+suivant = suivant->next;
+suivant->next = nouveau;
+return (nouveau);
 }
