eb79a53dfe125c47729211cfab2faae6ced4ca95
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-20 21:10:41 +0000

    stfu

---
+++ 2-add_node.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *add_node -  adds a new node at the beginning of a list_t list.
+ *@head : head of linked list
+ *@str : str of node
+ *Return: new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+int s = 0;
+list_t *nouveau = malloc(sizeof(*nouveau));
+if (head == NULL || nouveau == NULL)
+return (NULL);
+if (!str)
+{
+nouveau->str = NULL;
+nouveau->len = 0;
+}
+else
+{
+nouveau->str = strdup(str);
+while (*str != '\0')
+{
+s++;
+str++;
+}
+nouveau->len = s;
+}
+nouveau->next = *head;
+*head->next = nouveau;
+return (nouveau);
+
+}
