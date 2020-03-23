520ffdd890898b7e744496c2469903fae7939b4c
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-23 20:14:14 +0000

    second commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,23 +1,37 @@
 #include "lists.h"
 /**
- *insert_nodeint_at_index - function that inserts a new node at a given position
- *@head : pointer to list
- *@idx : index of node
- *@n : data(n) of node 
- *Return: node or null
+ * insert_nodeint_at_index - function to insert a node in a list at given index
+ * @head: pointer to the pointer to list
+ * @idx: index to insert node at
+ * @n: data to be inserted
+ * Return: node or NULL
  */
-listint_t *insert_nodeint_at_index(listint_t *head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int i;
-listint_t *nouveau = malloc(sizeof(*nouveau)), *suivant;
- if (nouveau == NULL)
-   return (NULL);
- nouveau->n = n;
- nouveau->next = NULL;
-for (i = 0; i < idx - 1; i++)
-head = head->next;
- suivant = head;
- head = nouveau;
- nouveau->next = suivant;
-  return (nouveau);
+listint_t *nouveau, *old;
+unsigned int i = 1;
+old = *head;
+nouveau = malloc(sizeof(nouveau));
+if (nouveau == NULL || head == NULL)
+return (NULL);
+nouveau->n = n;
+nouveau->next = NULL;
+if (idx == 0)
+{
+nouveau->next = *head;
+*head = nouveau;
+return (nouveau);
+}
+for (i = 0; i < idx - 1 && old; i++)
+old = old->next;
+if (old == NULL && i == idx)
+{
+old->next = nouveau;
+return (nouveau);
+}
+if (old == NULL)
+return (NULL);
+nouveau->next = old->next;
+old->next = nouveau;
+return (nouveau);
 }
