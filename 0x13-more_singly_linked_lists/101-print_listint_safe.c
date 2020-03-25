f82be0c05b5ed9ff927b5fdf8d7e19cb203dc85e
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-25 09:50:54 +0000

    4

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,19 +8,20 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-const  listint_t *zeb;
-int s = 0;
-while (head)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-zeb = head;
-head = head->next;
-s++;
-if ((void *)head > (void *)zeb)
-{
-printf("-> [%p] %d\n", (void *)head, head->n);
-break;
-}
-}
-return (s);
+	const  listint_t *zeb;
+	int s = 0;
+
+	while (head)
+	{
+		zeb = head;
+		head = head->next;
+		if ((void *)head > (void *)zeb)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		s++;
+	}
+	return (s);
 }
