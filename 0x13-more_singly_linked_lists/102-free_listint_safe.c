9ee51372733d3743b5de8a0b120e336efa2ee103
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-25 10:34:04 +0000

    5

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,8 +2,8 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- *print_listint_safe - fun that print all the elements of list int
- *@head : pointer of list
+ *free_listint_safe - fun that free
+ *@h : pointer of list
  *Return: nbr of elements in list
  */
 size_t free_listint_safe(listint_t **h)
@@ -15,7 +15,7 @@ size_t free_listint_safe(listint_t **h)
 	{
 		asup = *h;
 		*h = (*(h))->next;
-		if((void *)h >= (void *)asup)
+		if ((void *)h >= (void *)asup)
 		{
 			free(asup);
 			s++;
