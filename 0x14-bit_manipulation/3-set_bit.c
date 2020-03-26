44a4d144a066b713f12a446082bcfcfadea62265
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-03-26 20:52:54 +0000

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * set_bit - sets the value of a bit to 1 at given index
+ * @n:var pointer int
+ * @index:  starting from 0 of the bit you want to set
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int one = 1;
+
+	if (index > 64)
+		return (-1);
+	one <<= index;
+	*n |= one;
+	return (1);
+}
