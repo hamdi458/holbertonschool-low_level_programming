f4b683910e1e5daa5c7284eedb0db111ada62bba
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-05-02 19:03:27 +0000

    Sum list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * sum_dlistint - returns the sum of all the data n of a dlistint_t linked list.
+ * sum_dlistint - returns the sum of all the data n of a dlistint_t linked list
  * @head: var head
  * Return: returns the sum of all the data (n) of a dlistint_t linked list.
  */
