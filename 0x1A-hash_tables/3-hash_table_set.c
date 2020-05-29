500e4f70f0713218b75f4dae777756119c67cac2
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-05-29 13:29:41 +0000

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key || !value || !ht->array || strlen(key) == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
-	i = 0;
+	i = index;
 	while (ht->array[i])
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
