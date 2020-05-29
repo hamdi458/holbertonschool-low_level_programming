ae8fbec43410102a60400a8f3e1b4cdefa2f6536
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-05-29 13:25:11 +0000

    ht['betty'] = 'holberton'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,6 +2,7 @@
 #include <string.h>
 /**
  * hash_table_set - adds an element to the hash table.
+ * @ht: hash table you want to add or update the key/value to
  * @key: the key
  * @value: value associated with the key
  * Return: 1 if it succeeded, 0 otherwise
@@ -10,7 +11,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int i, index;
 	hash_node_t *new;
-	if (!ht || !key || !value || !ht->array || strlen(key) == 0 )
+
+	if (!ht || !key || !value || !ht->array || strlen(key) == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	while (ht->array[i])
