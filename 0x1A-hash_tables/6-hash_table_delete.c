43ad7387501fffd9a76ba38069c7170bbcdbaeba
hamdi458 <hamdi.ghorbel78@gmail.com>
2020-05-29 15:22:00 +0000

    del ht

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes a hash table.
+ * @ht: is the hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int i;
+	hash_node_t *node, *tmp;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
