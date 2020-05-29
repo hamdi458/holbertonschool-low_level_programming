d4a01eafa22e4512b5be54ec3c362c4df486bd39
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-05-29 13:24:00 +0000

    ht['betty'] = 'holberton'

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+#include <string.h>
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @key: the key
+ * @value: value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int i, index;
+	hash_node_t *new;
+	if (!ht || !key || !value || !ht->array || strlen(key) == 0 )
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	while (ht->array[i])
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = strdup(value);
+			return (1);
+		}
+		i++;
+	}
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+	{
+		return (0);
+	}
+	new->key = strdup(key);
+	new->value = strdup(value);
+	ht->array[index] = new;
+	return (1);
+
+}
