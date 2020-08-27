356f68a5fbca2d79e874c4ce20ce5ea8eec6e553
hamdi458 <hamdi.645@gmail.com>
2020-08-27 12:03:59 +0100

    betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,15 +10,15 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
-    if (array == NULL)
-        return (-1);
-    while (i < size)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-        i++;
-    }
-    return (-1);
+size_t i = 0;
+if (array == NULL)
+return (-1);
+while (i < size)
+{
+printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
+i++;
+}
+return (-1);
 }
