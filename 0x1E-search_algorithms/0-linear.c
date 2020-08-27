8c9bd0b80ee8575b05163cef5c308fc26cebf281
hamdi458 <hamdi.645@gmail.com>
2020-08-27 12:06:03 +0100

    test

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,7 +6,7 @@
  * @array: pointer to the first element of the array to search in
  * @size:  the number of elements in array
  * @value: value to search for
- * Return: index of value, or -1
+ * Return: index of value , or -1
  */
 int linear_search(int *array, size_t size, int value)
 {
